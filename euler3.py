60d808fe5358a149979cea0e5616fe9e047b740d
William Andrus <andrus_wj@hotmail.com>
2018-08-10 11:34:56 -0600

    Add comment on #3 with time and results

--- euler3.py
+++ euler3.py
@@ -1,6 +1,9 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143 ?
 
+#{71: 8462696833, 839: 716151937, 1471: 408464633, 6857: 87625999}
+#9085.9359664917 seconds ... too long
+
 from time import time
 from math import sqrt
 
